aeeebb0950f3ff6048602c37d98037512cc4e5b3
Samuel <samueloluwatommzy@gmail.com>
2018-03-12 15:44:16 +0100

    code refactor

--- index.js
+++ index.js
@@ -7,19 +7,8 @@ let app = express()
 let port = process.env.port | 8000
 app.use(bodyParser.json());
 app.use(cors());
-
-//file storage to disk using diskStorage engine
-let storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,'../uploads')
-    },
-    filename: function(req,file,cb){
-        cb(null,file.fieldname +'-'+ Date.now() + '.' + 
-        file.originalname.split('.')[file.originalname.split('.').length -1]);
-
-    }
-})
-let upload = multer({storage:storage}).single('file');
+let apiRoute = require('./route/api');
+app.use('/api' , apiRoute);
 app.listen(port,function(err){
     if(err) return;
 console.log("app listening on port " + port);
