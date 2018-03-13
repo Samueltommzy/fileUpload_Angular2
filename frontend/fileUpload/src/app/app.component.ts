764dd41d28836a70fb39f7ae59f4848631f8d621
Samuel <samueloluwatommzy@gmail.com>
2018-03-13 13:59:17 +0100

    update on the app module

--- frontend/fileUpload/src/app/app.component.ts
+++ frontend/fileUpload/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FileUploader} from 'ng2-file-upload';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +8,14 @@ import {FileUploader} from 'ng2-file-upload';
 })
 export class AppComponent {
   title = 'File Upload';
- public:FileUploader = new FileUploader({url:'localhost:8000/api/upload'});
+ public uploader :FileUploader = new FileUploader({url:'localhost:8000/api/upload'});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
+ 
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+}
+public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
 }
